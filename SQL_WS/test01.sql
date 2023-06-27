5152364c20076e21809944b9daa4146064a3ce89
offpython <offpython@gmail.com>
2023-06-28 01:21:49 +0900

    update

---
+++ SQL_WS/test01.sql
@@ -0,0 +1,6 @@
+#MySQL 접속해서 실행
+CREATE TABLE police_station
+(
+    name varchar(16),
+    address varchar(128)
+);
---
+++ SQL_WS/test02.sql
@@ -0,0 +1,9 @@
+#터미널단계(외부)에 접속해서 실행
+CREATE TABLE crime_status
+(
+    year YEAR,
+    police_station varchar(8),
+    crime_stype varchar(16),
+    status_type char(2),
+    case_number int
+);
