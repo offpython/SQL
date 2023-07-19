323ce73f4a848a022e04eeb8190e693b792e7697
offpython <offpython@gmail.com>
2023-07-19 12:50:17 +0900

    update

---
+++ SQL_WS/test04.sql
@@ -0,0 +1,4 @@
+insert into sql_file VALUES(1, "test01.sql");
+insert into sql_file VALUES(2, "test02.sql");
+insert into sql_file VALUES(3, "test03.sql");
+insert into sql_file VALUES(4, "test04.sql");
